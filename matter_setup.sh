45a2e0cbc1b7df5027d33f6b5e4d87525412adb4
mikaelajiwidodo <mikaelajiwidodo@realtek-sg.com>
2026-04-29 14:55:26 +0800

    [amebadplus][amebalite][amebasmart][sdk] Added Matter v1.5 Support
    * Modified Menuconfig and Makefile scripts to support Matter sub-module
    * Added ble_matter_adapter_peripheral example to the bluetooth folder
    * LwIP modifications
        * Added extern struct netif xnetif[]; to lwip_netconf.h
        * Included lwipopts_matter.h in lwipopts.h
    * FreeRTOS modifications
        * Added xPortResetHeapMinimumEverFreeHeapSize API
        * Added lib_freertos_smp_matter.a of AmebaSmart
    * TrustZone modifications
        * Added CONFIG_TZ_S_SIZE in the Menuconfig to modify TZ_S_SIZE of dplus and lite ld file
        * Added CONFIG_SECURE_HEAP_SIZE in the Menuconfig to modify secureconfigTOTAL_SRAM_HEAP_SIZE of FreeRTOSConfig.h
        * Modified component/soc/amebasmart/atf/
    * SoC header files modifications
        * Removed `IN/OUT/INOUT` macros out of header files
        * Modified `PeripheralNames.h` to support Matter sub-module
        * Added `extern "C"` to `ameba_reset.h` and `strproc.h` if `__cplusplus` is defined
    * Added `matter_setup.sh` script to help setting up Matter

---
+++ matter_setup.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+if [ "$#" -lt 2 ]; then
+  echo "Usage: $0 <IC type> <Matter version>"
+  echo "IC type       : ameba-rtos"
+  echo "Matter version: v1.5"
+  exit 1
+fi
+
+AMEBA="$1"
+MATTER_VER="$2"
+
+files_to_delete=(
+	"$PWD/component/soc/amebadplus/cmsis/cmsis.h"
+	"$PWD/component/soc/amebadplus/cmsis/cmsis_nvic.h"
+	"$PWD/component/soc/amebadplus/cmsis/core_cache.h"
+	"$PWD/component/soc/amebadplus/cmsis/core_cm0plus.h"
+	"$PWD/component/soc/amebadplus/cmsis/core_cm4_simd.h"
+	"$PWD/component/soc/amebadplus/cmsis/core_cmFunc.h"
+	"$PWD/component/soc/amebadplus/cmsis/core_cmInstr.h"
+	"$PWD/component/soc/amebadplus/cmsis/mpu.h"
+	"$PWD/component/soc/amebadplus/cmsis/mpu_config.h"
+	"$PWD/component/soc/amebadplus/cmsis/mpu_config_ns.h"
+	"$PWD/component/soc/amebadplus/cmsis/mpu_config_s.h"
+)
+
+files_to_set_executable=(
+  "$PWD/amebasmart_gcc_project/project_hp/asdk/gnu_utility/image_tool/imagetool.sh"
+  "$PWD/amebasmart_gcc_project/project_hp/asdk/gnu_utility/image_tool/elf2bin"
+  "$PWD/amebadplus_gcc_project/menuconfig/scripts/Menuconfig"
+  "$PWD/amebasmart_gcc_project/menuconfig/scripts/Menuconfig"
+)
+
+delete_files() {
+  for file_path in "${files_to_delete[@]}"; do
+    if [ -e "$file_path" ]; then
+      rm "$file_path"
+      echo "File $file_path removed."
+    else
+      echo "File $file_path does not exist."
+    fi
+  done
+}
+
+set_executable_files() {
+  for file_path in "${files_to_set_executable[@]}"; do
+    if [ -e "$file_path" ]; then
+      chmod u+x "$file_path"
+      echo "File $file_path is now executable."
+    else
+      echo "File $file_path does not exist."
+    fi
+  done
+}
+
+case "$AMEBA" in
+  ameba-rtos)
+    echo "Configuring for $AMEBA"
+    delete_files
+    set_executable_files
+    ;;
+  *)
+    echo "Invalid IC type argument. Expected 'ameba-rtos'."
+    exit 1
+    ;;
+esac
+
+case "$MATTER_VER" in
+  v1.5)
+    echo "Matter Version is set to $MATTER_VER"
+    ;;
+  *)
+    echo "Invalid Matter version argument. Expected 'v1.5'."
+    exit 1
+    ;;
+esac
+
+if [ ! -d third_party ];then
+    mkdir third_party
+else
+    rm third_party/connectedhomeip
+fi
+
+cd third_party
+rm -rf connectedhomeip
+ln -s ../../connectedhomeip connectedhomeip
+
+cd ../
+
+case "$AMEBA" in
+  ameba-rtos)
+    if [ ! -d component/application/matter ] || [ -z "$(find component/application/matter -mindepth 1)" ]; then
+      mkdir -p component/application/matter
+      git clone https://github.com/Ameba-AIoT/ameba-rtos-matter.git component/application/matter -b ameba-rtos/release/$MATTER_VER
+    fi
+    ;;
+  *)
+    echo "Invalid argument. Expected 'ameba-rtos'."
+    exit 1
+    ;;
+esac
+echo "Matter setup complete"
